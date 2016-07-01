50a0a0751b8dffd38740447b1c59f338006bf813
Marcel Michau <Marcel.Michau@investec.co.za>
2016-07-01 11:33:31 +0200

    Added production webpack config with optimizations & style splitting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Component from './component';
+import styles from './styles/styles.css';
 
 ReactDOM.render(<Component greeting='Ole!'/>, document.getElementById('app'));
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,3 @@
-
 import webpack from 'webpack';
 import path from 'path';
 
@@ -31,12 +30,21 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+      {
+        test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'], query: {
+          presets: ['es2015', 'react'],
+          env: {
+            development: {
+              presets: ["react-hmre"]
+            }
+          }
+        }
+      },
+      { test: /(\.css)$/, loaders: ['style', 'css'] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
     ]
   }
 };
